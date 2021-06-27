0250e580c28566fb1d19ecbfa818f9ccdc36f320
S-codes14 <81442054+S-codes14@users.noreply.github.com>
2021-06-27 05:30:21 +0000

    Deploying to gh-pages from  @ d90336fc04489d4764ff810e0638371be5580ef0 🚀

---
+++ precache-manifest.15d590e2a2116fb5301f79b5e74f55f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4650bdadbfb95e26c715c2407c19f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3abc886f67c238c144aa",
+    "url": "/static/css/main.67c8d63e.chunk.css"
+  },
+  {
+    "revision": "a5f813aa5a1990ce9ad7",
+    "url": "/static/js/2.fdb7aa78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdb7aa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abc886f67c238c144aa",
+    "url": "/static/js/main.ad63bd20.chunk.js"
+  },
+  {
+    "revision": "bc0c0da8b2f8f33173d5",
+    "url": "/static/js/runtime-main.7b78168b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e183ca850a0ab128a76da037cd05bd04",
+    "url": "/static/media/hero.e183ca85.jpeg"
+  }
+]);
