5c1b373df24385b7f304fbb0fad3b303e877fa23
S-codes14 <smlmnguni14@gmail.com>
2021-06-27 06:06:25 +0200

    added the hero page

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,22 +1,23 @@
 import React, {Component} from "react";
-import Header from "../components/header/Header";
-import Greeting from "./greeting/Greeting";
-import Skills from "./skills/Skills";
-import StackProgress from "./skillProgress/skillProgress";
-import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
-import StartupProject from "./StartupProjects/StartupProject";
-import Achievement from "./achievement/Achievement";
-import Blogs from "./blogs/Blogs";
-import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
-import Education from "./education/Education";
-import Top from "./topbutton/Top";
+// import Header from "../components/header/Header";
+// import Greeting from "./greeting/Greeting";
+// import Skills from "./skills/Skills";
+// import StackProgress from "./skillProgress/skillProgress";
+// import WorkExperience from "./workExperience/WorkExperience";
+// import Projects from "./projects/Projects";
+// import StartupProject from "./StartupProjects/StartupProject";
+// import Achievement from "./achievement/Achievement";
+// import Blogs from "./blogs/Blogs";
+// import Footer from "../components/footer/Footer";
+// import Talks from "./talks/Talks";
+// import Podcast from "./podcast/Podcast";
+// import Education from "./education/Education";
+// import Top from "./topbutton/Top";
+import Hero from "./hero"
 import Twitter from "./twitter-embed/twitter";
 import {StyleProvider} from "../contexts/StyleContext";
 import "./Main.css";
-import Profile from "./profile/Profile";
+// import Profile from "./profile/Profile";
 
 export default class Main extends Component {
   constructor(props) {
@@ -45,22 +46,23 @@ export default class Main extends Component {
         <StyleProvider
           value={{isDark: this.state.isDark, changeTheme: this.changeTheme}}
         >
-          <Header />
+        <Hero />
+          {/* <Header />
           <Greeting />
           <Skills />
           <StackProgress />
           <Education />
-          <WorkExperience />
+          <WorkExperience /> */}
           {/* <Projects /> */}
-          <StartupProject />
+          {/* <StartupProject />
           <Achievement />
           <Blogs />
-          <Talks />
+          <Talks /> */}
           <Twitter />
-          <Podcast />
+          {/* <Podcast />
           <Profile />
           <Footer />
-          <Top />
+          <Top /> */}
         </StyleProvider>
       </div>
     );
---
+++ src/containers/hero/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+
+function index() {
+    return (
+        <div class="Hero__HeroContainer-b98oqm-0 kvYdOK">
+        <div>
+          <h1>We Are CREATIVITY</h1>
+          <a href="start/" class="Hero__CTAButton-b98oqm-1 gJuuYA"
+            >Let's start</a
+          >
+        </div>
+        <img
+          src="_next/static/images/hero-7ad757c5ecd717f796c62ff4a0cb81d6.png"
+        />
+      </div>
+    )
+}
+
+export default index
